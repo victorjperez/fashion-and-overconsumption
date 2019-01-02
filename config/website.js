906d090727fe3ed77b42a4703cea18c6e3e3ddf3
Victor Perez <vplocked@Victors-MBP.fios-router.home>
2019-01-01 19:29:47 -0500

    Updated site url

--- config/website.js
+++ config/website.js
@@ -1,11 +1,11 @@
 const tailwind = require("../tailwind");
 
 module.exports = {
-  pathPrefix: "/fashionandoverconsumption", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: "/fashion-and-overconsumption", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: "Fashion & Overconsumption", // Navigation and Site Title
   siteTitleAlt: "Fashion & Overconsumption", // Alternative Site title for SEO
-  siteUrl: "https://portfolio-cara.netlify.com", // Domain of your site. No trailing slash!
+  siteUrl: "https://victorjperez.github.io", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
   siteLogo: "/logos/logo-1024.png", // Used for SEO and manifest
   siteDescription: "Why we buy so many clothes, and what that means for us.",
